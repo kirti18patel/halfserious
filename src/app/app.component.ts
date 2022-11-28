cb1b0fe2e3e88e63072de53ad2b5f61cc1212c6d
kirti18patel <kirti18patel@yahoo.com>
2022-11-28 11:31:55 -0500

    half serious assessment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from './service/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'halfserious';
+  title = 'HalfSerious';
+  visible:boolean = false;
+  starships = Object();
+  
+  constructor(private api:ApiService) {}
+
+  ngOnInit() {
+    this.api.getSWData().subscribe((data)=>{
+      this.starships = data;    
+    });
+  }
+  
+  onclick()
+  {
+    this.visible = !this.visible
+  }
 }
